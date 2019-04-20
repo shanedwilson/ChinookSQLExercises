912c84f6ee6c84d8b3d550b98e9b5a37eaaefd4a
Shane D Wilson <me@shanedwilson.com>
2019-04-20 09:56:46 -0500

    fixes

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,6 +1,6 @@
 Select Top 5 t.Name, Count(t.TrackId) #OfSales
 From Track t
 Join InvoiceLine il On t.TrackId = il.TrackId
-Join Invoice i On i.InvoiceId = il.InvoiceLineId
+Join Invoice i On i.InvoiceId = il.InvoiceId
 Group By t.Name
 Order By #OfSales Desc
