57e95da9a5b045b39abc689fe88022c4914779c9
Shane D Wilson <me@shanedwilson.com>
2019-04-20 10:22:50 -0500

    fixes

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,6 +1,6 @@
 Select Top 5 Count(il.TrackId) Sales, t.Name
 From InvoiceLine il
-Join Invoice i On i.InvoiceId = il.InvoiceLineId
+Join Invoice i On i.InvoiceId = il.InvoiceId
 Join Track t On t.TrackId = il.TrackId
 Where Year(i.InvoiceDate) = '2013'
 Group By t.Name
